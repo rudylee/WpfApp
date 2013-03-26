c9e5816e3082d94e21fd47a1fabb32ca3504b4b2
Rudy Lee <rudylee1988@gmail.com>
2013-03-26 17:40:19 +1100

    add another checking for file does not exist

--- WpfApp.Core/AppUpdate.cs
+++ WpfApp.Core/AppUpdate.cs
@@ -7,7 +7,6 @@ using System.Net;
 using System.Configuration;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Forms;
 using Button = System.Windows.Controls.Button;
 using ProgressBar = System.Windows.Controls.ProgressBar;
 
@@ -21,16 +20,16 @@ namespace WpfApp.Core
         private readonly WebClient _webClient = new WebClient();
         private readonly string _videoFolder;
         private List<string> _videosToDownload = new List<string>();
-        private int _totalVideosToDownload;
 
         public AppUpdate(TextBlock progressTextBlock, Button goToApp, ProgressBar progressBar)
         {
-            _progressTextBlock = progressTextBlock;
-            _goToApp = goToApp;
-            _progressBar = progressBar;
             var xmlUrl = new Uri(ConfigurationManager.AppSettings.Get("xmlURL"));
             var xmlFile = ConfigurationManager.AppSettings.Get("xmlFile");
             _videoFolder = ConfigurationManager.AppSettings.Get("videoFolder");
+
+            _progressTextBlock = progressTextBlock;
+            _goToApp = goToApp;
+            _progressBar = progressBar;
 
             _progressTextBlock.Text = "Synchronizing with main server...";
             _webClient.DownloadFileAsync(xmlUrl, xmlFile);
@@ -41,16 +40,33 @@ namespace WpfApp.Core
         {
             _webClient.DownloadFileCompleted -= XmlDownloadCompleted;
             _videosToDownload = GetVideosListToDownload();
-            _totalVideosToDownload = _videosToDownload.Count;
             
             if (_videosToDownload.Count > 0)
             {
                 var remoteVideoUrl = ConfigurationManager.AppSettings.Get("remoteVideoURL");
                 var videoUri = new Uri(remoteVideoUrl + _videosToDownload[0]);
 
-                _webClient.DownloadFileCompleted += VideoDownloadCompleted;
-                _webClient.DownloadProgressChanged += VideoProgressChanged;
-                _webClient.DownloadFileAsync(videoUri, _videoFolder + _videosToDownload[0]);
+                // Check whether file exists
+                var request = (HttpWebRequest) WebRequest.Create(videoUri);
+                try
+                {
+                    using (var response = (HttpWebResponse) request.GetResponse())
+                    {
+                        if (response.StatusCode == HttpStatusCode.OK)
+                        {
+                            _webClient.DownloadFileCompleted += VideoDownloadCompleted;
+                            _webClient.DownloadProgressChanged += VideoProgressChanged;
+                            _webClient.DownloadFileAsync(videoUri, _videoFolder + _videosToDownload[0]);
+                        }
+                        response.Close();
+                    }
+                }
+                catch (WebException exception)
+                {
+                    _goToApp.Visibility = Visibility.Visible;
+                    MessageBox.Show(
+                        "There is an error when downloading the files. You might want to contact your administrator");
+                }
             }
             else
             {
